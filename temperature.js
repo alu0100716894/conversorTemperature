54455300a242b3126a7a2ed5732d6ed8f52cebe9
Manuel Alejandro Rodríguez Santana <alu0100716894@ull.edu.es>
2015-02-04 15:17:31 +0000

    update

---
+++ temperature.js
@@ -0,0 +1,27 @@
+"use strict"; //modo estricto para los navegadores que lo permitan
+function calculate(){
+  var result;
+  var original = document.getElementById("origin");
+  var temp = original.value;
+  var regexp = /[-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
+
+  var m = temp.match(regexp);
+
+  if(m) {
+    var num = m[1];
+    var type = m[2];
+    num = parsefloat(num);
+    if (type == 'c' || type == 'C'){
+      result = (num * 9/5) + 32;
+      result = result.toFixed(1) + " Farenheit";
+    }
+    else {
+      result = (num - 32) * 5/9;
+      result = result.toFixed(1) + " Celsius";
+    }
+    converted.innerHTML = result;
+  }
+  else {
+    converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+  }
+}
