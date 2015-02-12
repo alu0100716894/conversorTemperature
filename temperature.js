2d71601a12045f1d1aeab0720584a4966558d43e
Manuel Alejandro Rodríguez Santana <alu0100716894@ull.edu.es>
2015-02-12 09:26:04 +0000

    adding kelvin

--- temperature.js
+++ temperature.js
@@ -1,10 +1,11 @@
 "use strict"; //modo estricto para los navegadores que lo permitan
 function calculate(){
   var result;
+  var result2;
   var original = document.getElementById("original");
   var temp = original.value;
   //var regexp = /([-+]?\d+(?:\.\d*)?)\s*([fFcC])/;
-  var regexp = /^\s*([-+]?\d+(?:\.\d*)?º?(?:[eE][-+]?\d+)?)\s*([cCfF])$/
+  var regexp = /^\s*([-+]?\d+(?:\.\d*)?º?(?:[eE][-+]?\d+)?)\s*([cCfFkK])$/
 
   var m = temp.match(regexp);
 
@@ -15,14 +16,26 @@ function calculate(){
     if (type == 'c' || type == 'C'){
       result = (num * 9/5) + 32;
       result = result.toFixed(1) + " Farenheit";
+      result2 = num + 273.15;
+      result2 = result2.toFixed(1) + " Kelvin"
     }
-    else {
+    else if (type == 'f' || type == 'F'){
       result = (num - 32) * 5/9;
       result = result.toFixed(1) + " Celsius";
+      result2 = ((num - 32) / 1.8) + 273;
+      result2 = result2.toFixed(1) + " Kelvin"
+    }
+    else{
+      result = num - 273.15;
+      result2 = (result * 9/5) + 32;
+      result = result.toFixed(1) + " Celsius";
+      result2 = result2.toFixed(1) + " Farenheit"
     }
     converted.innerHTML = result;
+    converted2.innerHTML = result2;
   }
   else {
     converted.innerHTML = "ERROR! Try something like '-4.2C' instead";
+    converted2.innerHTML = "ERROR! Try something like '-4.2C' instead";
   }
 }
