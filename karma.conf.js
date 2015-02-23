f9a7998fbfd7db6737e3299410714450158a64dc
Manuel Alejandro Rodríguez Santana <alu0100716894@ull.edu.es>
2015-02-23 12:28:23 +0000

    karma and blanket-6

--- karma.conf.js
+++ karma.conf.js
@@ -26,7 +26,7 @@ module.exports = function(config) {
 
     // preprocess matching files before serving them to the browser
     // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: 'testk.html': ['html2js']{
+    preprocessors: 'tests/tests.html': ['html2js']{
     },
 
 
