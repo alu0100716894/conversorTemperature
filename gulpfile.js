71cf9dd1a7e156706a379b6bc676ad4056f4c236
Manuel Alejandro Rodríguez Santana <alu0100716894@ull.edu.es>
2015-02-26 09:36:54 +0000

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@
 */
 var gulp = require('gulp'),
   concat = require('gulp-concat'),
+  gutil   = require('gulp-util'),
   uglify = require('gulp-uglify');
+var karma   = require('gulp-karma');
 
 /*
 * Configuración de la tarea 'minidy'
@@ -13,4 +15,25 @@ gulp.task('minify', function () {
   .pipe(uglify())
   .pipe(gulp.dest('js/build/'))
   .pipe(gulp.dest('minified'))
+});
+
+gulp.task('test', function() {
+  // Be sure to return the stream
+  return gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'run'
+    }))
+    .on('error', function(err) {
+      // Make sure failed tests cause gulp to exit non-zero
+      throw err;
+    });
+});
+
+gulp.task('default', function() {
+  gulp.src([])
+    .pipe(karma({
+      configFile: 'karma.conf.js',
+      action: 'watch'
+    }));
 });
